9272e895d1a5c1c21da21b2f468dbcc9030e7e25
Nyikovics Máté <nyikovicsmate@gmail.com>
2019-12-04 04:41:48 +0100

    Extracted batch size into a variable.

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
     logger.info(f"Started at: {time.time()}")
     idm = ImageDatasetManager(dataset_src_dir="raw_images", dataset_dst_dir="preprocessed_images")
     idm.preprocess(overwrite=False)
-    batch_generator = idm.train_batch_generator(64)
-    network = PixelwiseA3CNetwork(input_shape=(64, 70, 70, 1))
+    batch_size = 32
+    batch_generator = idm.train_batch_generator(batch_size)
+    network = PixelwiseA3CNetwork(input_shape=(batch_size, 70, 70, 1))
     network.train(batch_generator=batch_generator,
                   epochs=10000)
