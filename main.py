c85910ac4db206fa99ada22f714d0ba3774dabb4
Nyikovics Máté <nyikovicsmate@gmail.com>
2019-12-24 12:50:08 +0100

    Refactored logic into a Runner class.

--- main.py
+++ main.py
@@ -5,33 +5,29 @@ from image_dataset_manager import ImageDatasetManager
 from pixelwise_a3c_network import PixelwiseA3CNetwork
 
 
-def train():
-    # logger.info(f"Started at: {time.time()}")
+class Runner:
     w, h = (70, 70)
     idm = ImageDatasetManager(dataset_src_dir="raw_images",
                               dataset_dst_dir="preprocessed_images",
                               dst_shape=(w, h),
                               split_ratio=0.9)
     idm.preprocess(overwrite=False)
-    batch_size = 64
-    batch_generator = idm.train_batch_generator(batch_size)
-    network = PixelwiseA3CNetwork(input_shape=(batch_size, w, h, 1))
-    network.train(batch_generator=batch_generator,
-                  epochs=1000,
-                  resume_training=False)
 
+    @staticmethod
+    def train():
+        batch_size = 64
+        batch_generator = Runner.idm.train_batch_generator(batch_size, randomize=True)
+        network = PixelwiseA3CNetwork(input_shape=(batch_size, Runner.w, Runner.h, 1))
+        network.train(batch_generator=batch_generator,
+                      epochs=1000,
+                      resume_training=False)
 
-def predict():
-    w, h = (70, 70)
-    idm = ImageDatasetManager(dataset_src_dir="raw_images",
-                              dataset_dst_dir="preprocessed_images",
-                              dst_shape=(w, h),
-                              split_ratio=0.9)
-    idm.preprocess(overwrite=False)
-    batch_size = 64
-    batch_generator = idm.train_batch_generator(batch_size)
-    network = PixelwiseA3CNetwork(input_shape=(batch_size, w, h, 1))
-    network.predict(batch_generator)
+    @staticmethod
+    def predict():
+        batch_size = 64
+        batch_generator = Runner.idm.train_batch_generator(batch_size, randomize=False)
+        network = PixelwiseA3CNetwork(input_shape=(batch_size, Runner.w, Runner.h, 1))
+        network.predict(batch_generator)
 
 
 if __name__ == '__main__':
@@ -41,8 +37,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.train:
-        train()
+        Runner.train()
     if args.predict:
-        predict()
+        Runner.predict()
     else:
         logger.info("No mode selected, exiting.")
